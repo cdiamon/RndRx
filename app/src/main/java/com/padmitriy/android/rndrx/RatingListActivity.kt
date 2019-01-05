4b1184d609a2c763e38e8533aa9ffa392a4e3f3f
padmitriy <padmitriy@gmail.com>
2019-01-05 21:09:54 +0300

    init commit

---
+++ app/src/main/java/com/padmitriy/android/rndrx/RatingListActivity.kt
@@ -0,0 +1,12 @@
+package com.padmitriy.android.rndrx
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class RatingListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rating_list)
+    }
+}
